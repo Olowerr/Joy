55194583f454fc44fb77fabc7aca12c94b088fe4
Olowerr <oliver.klingspor@hotmail.com>
2022-03-30 14:52:33 +0200

    marcus

--- Joy/Main.cpp
+++ Joy/Main.cpp
@@ -2,8 +2,36 @@
 
 #include<d3d11.h>
 
+
+struct  Backend
+{
+	ID3D11Device* dev;
+};
+
+class GameClass 
+{
+public:
+	static Backend& Get()
+	{
+		static GameClass instance;
+		return instance.system;
+	}
+
+
+private:
+	GameClass();
+	Backend system;
+
+public:
+	GameClass(const GameClass& other) = delete;
+	GameClass& operator=(const GameClass& other) = delete;
+	GameClass(GameClass&& other) = delete;
+
+};
+
 int APIENTRY wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE prevHInstance, _In_ LPWSTR lpwstr, _In_ int showCmd)
 {
-	ID3D11Device* qwe;
+	GameClass ägde;
+
 	return 0;
 }
