6e51da7c26b34af88623f688b48d6d559da9d5bb
ADAM ÅKESSON <adao20@student.bth.se>
2022-03-30 15:18:48 +0200

    devvvv

--- Joy/Main.cpp
+++ Joy/Main.cpp
@@ -1,37 +1,10 @@
 #include <Windows.h>
-
 #include<d3d11.h>
-
-
-struct  Backend
-{
-	ID3D11Device* dev;
-};
-
-class GameClass 
-{
-public:
-	static Backend& Get()
-	{
-		static GameClass instance;
-		return instance.system;
-	}
-
-
-private:
-	GameClass();
-	Backend system;
-
-public:
-	GameClass(const GameClass& other) = delete;
-	GameClass& operator=(const GameClass& other) = delete;
-	GameClass(GameClass&& other) = delete;
-
-};
+#include "Backend.h"
 
 int APIENTRY wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE prevHInstance, _In_ LPWSTR lpwstr, _In_ int showCmd)
 {
-	GameClass::Get().dev;
+	Backend::GetDevice();
 
 	return 0;
 }
