917b0ceef91880c85bebdceaf1d42f535e0ede1b
ADAM ÅKESSON <adao20@student.bth.se>
2022-03-30 15:24:39 +0200

    dada

--- Joy/Main.cpp
+++ Joy/Main.cpp
@@ -1,5 +1,5 @@
 #include <Windows.h>
-#include<d3d11.h>
+#include <d3d11.h>
 #include "Backend.h"
 
 int APIENTRY wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE prevHInstance, _In_ LPWSTR lpwstr, _In_ int showCmd)
