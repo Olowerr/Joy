cc3e54c334f783b187a54394e6b7b9422588f04c
Olowerr <oliver.klingspor@hotmail.com>
2022-03-30 14:14:28 +0200

    123

--- Joy/Main.cpp
+++ Joy/Main.cpp
@@ -1,7 +1,9 @@
 #include <Windows.h>
 
+#include<d3d11.h>
+
 int APIENTRY wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE prevHInstance, _In_ LPWSTR lpwstr, _In_ int showCmd)
 {
-	//aaaaaaaaaaaaaaaa
+	ID3D11Device* qwe;
 	return 0;
 }
