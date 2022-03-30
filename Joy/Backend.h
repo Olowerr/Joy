be99d5e96b0de1ec106d02a07f4c488193ac2156
ADAM ÅKESSON <adao20@student.bth.se>
2022-03-30 15:13:25 +0200

    Lez go

---
+++ Joy/Backend.h
@@ -0,0 +1,35 @@
+#pragma once
+#include <Windows.h>
+#include <d3d11.h>
+
+Backend Backend::system;
+
+class Backend
+{
+public:
+
+	static bool initiate(HINSTANCE hInst, int showCmd)
+	{
+		// DO THINGS
+	}
+
+	static ID3D11Device* Get()
+	{
+		return system.dev;
+	}
+
+private:
+
+	Backend();
+
+	static Backend system;
+	ID3D11Device* dev;
+	ID3D11DeviceContext* devCont;
+
+public:
+
+	Backend(const Backend& other) = delete;
+	Backend& operator=(const Backend& other) = delete;
+	Backend(Backend&& other) = delete;
+
+};
--- Joy/Main.cpp
+++ Joy/Main.cpp
@@ -31,7 +31,7 @@ public:
 
 int APIENTRY wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE prevHInstance, _In_ LPWSTR lpwstr, _In_ int showCmd)
 {
-	GameClass duegd;
+	GameClass::Get().dev;
 
 	return 0;
 }
