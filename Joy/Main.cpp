3bc95fbafb401dcc18d6d900ab8e3f4990ccd86f
Olowerr <oliver.klingspor@hotmail.com>
2022-03-30 14:07:28 +0200

    Update Main.cpp

--- Joy/Main.cpp
+++ Joy/Main.cpp
@@ -1,6 +1,6 @@
 #include <Windows.h>
 
-int APIENTRY wWinMain()
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE prevHInstance, _In_ LPWSTR lpwstr, _In_ int showCmd)
 {
 	//aaaaaaaaaaaaaaaa
 	return 0;
