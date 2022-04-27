0fe3db073df65fb67aafaf72d3ef938a01b6f656
Emilio Håkansson <emilio.hakansson@gmail.com>
2022-04-27 13:53:56 +0200

    same as previous

--- Joy/Playground.cpp
+++ Joy/Playground.cpp
@@ -11,30 +11,33 @@ void testScene::Load()
     ID3D11DeviceContext* devContext = Backend::GetDeviceContext();
 
     meshStorage.LoadAll();
-    joy = new Character(meshStorage.GetMesh(0));
+
+    // Joy should always be first in the array from mesh storage
+    joy = new Character(meshStorage.GetMesh(0)); 
     collTest = new Object(meshStorage.GetMesh(1));
-    //test->SetPosition(0.0f, 0.2f, .0f);
-    bg = new Object(meshStorage.GetMesh(1));
     ground = new Object(meshStorage.GetMesh(2));
+
+
+    //Camera recives wich object to look at
     camera = new CharacterCamera(*joy);
+
+
     objRender.AddObject(ground);
     objRender.AddObject(joy);
-    objRender.AddObject(bg);
-    ground->SetPosition(0.0f, -2.0f, 0.0f);
-    bg->SetPosition(-10.0f, 1.0f, -5.0f);
-    collTest->SetPosition(-20.0f, 0.0f, 0.0f);
-    //bg->SetScale(1.f / 23.f);
 
-  //  bg->Scale(2);
+
+    ground->SetPosition(0.0f, -2.0f, 0.0f);
+    collTest->SetPosition(-20.0f, 0.0f, 0.0f);
 
     viewAndProj = camera->GetViewAndProj();
 
     Backend::CreateDynamicCBuffer(&camCb, &viewAndProj, 64);
     Backend::UpdateBuffer(camCb, &viewAndProj, 64);
     devContext->VSSetConstantBuffers(1, 1, &camCb); 
-    objRender.AddObject(collTest);
 
+    objRender.AddObject(collTest);
     objRender.CreateCharacterDecal(joy);
+    
     devContext->PSSetConstantBuffers(0, 1, objRender.getDecalBuffer());
 }
 
@@ -42,56 +45,48 @@ void testScene::Shutdown()
 {
     objRender.Clear();
     meshStorage.UnLoadAll();
+
     joy->Shutdown();
-    bg->Shutdown();
     ground->Shutdown();
+    collTest->Shutdown();
+
     delete ground;
     delete camera;
-    collTest->Shutdown();
     delete collTest;
     delete joy;
-    delete bg;
 }
 
 SceneState testScene::Update()
 {
-
-
     ID3D11DeviceContext* devContext = Backend::GetDeviceContext();
     viewAndProj = camera->GetViewAndProj();
     devContext->VSSetConstantBuffers(1, 1, &camCb);
     Backend::UpdateBuffer(camCb, &viewAndProj, 64);
 
-
+    //Camera functions
     camera->UpdateCam();
     camera->SetView();
 
-
-
+    //Collision
     joy->SetCanJump(false);
     joy->setCollidedY(coll.getCollidedY());
-    if (coll.hitObject(joy, collTest))
+    if (coll.HitObject(joy, collTest))
         joy->setSpeedZero();
-    if (coll.hitObject(joy, ground))
+    if (coll.HitObject(joy, ground))
         joy->SetCanJump(coll.GetStopFall());
-    if (coll.hitObject(joy, collTest))
+    if (coll.HitObject(joy, collTest))
         joy->SetCanJump(coll.GetStopFall());
     
     coll.collided(joy, collTest);
 
-
-
-
-
-
-
+    //Joy functions
     joy->Jump();
     joy->Move();
     joy->Respawn();
+
+    //Decal
     objRender.UpdateCharacterDecal(joy);
 
-    //Collision
-    
 
     return SceneState::Unchanged;
 }
