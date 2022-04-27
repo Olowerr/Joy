c828d7d1658b18638fe5dc92774a994e328ff2cd
Alexandra <allb16@student.bth.se>
2022-04-27 15:37:48 +0200

    wellwellwelllll

--- Joy/Points.cpp
+++ Joy/Points.cpp
@@ -3,4 +3,23 @@
 Points::Points(Pickup* pickups_in)
 	: pickups(pickups_in)
 {
+	
+}
+
+void Points::StartTimer()
+{
+	startTime = std::chrono::steady_clock::now();
+}
+
+void Points::CountTimer()
+{
+}
+
+void Points::StopTimer()
+{
+	endTime = std::chrono::steady_clock::now();
+
+	std::chrono::duration<double> timerr = std::chrono::duration_cast<std::chrono::duration<double>>(endTime - startTime);
+	int q = 0;
+
 }
--- Joy/Points.h
+++ Joy/Points.h
@@ -5,8 +5,11 @@ class Points
 {
 public:
 	Points(Pickup* pickups_in);
+	void StartTimer();
+	void CountTimer();
+	void StopTimer();
 private:
 	Pickup* pickups;
-
-
+	std::chrono::steady_clock::time_point startTime;
+	std::chrono::steady_clock::time_point endTime;
 };
