3b8ff38a555930ed7b4db03df406a2eb2774ea84
ADAM ÅKESSON <adao20@student.bth.se>
2022-04-29 14:53:36 +0200

    Latest Merge

--- Joy/Playground.h
+++ Joy/Playground.h
@@ -3,33 +3,37 @@
 #include "Character.h"
 #include "CharacterCamera.h"
 #include "Collision.h"
+<<<<<<< HEAD
+=======
+#include "FreelookCamera.h"
+#include "LightHandler.h"
+>>>>>>> master
 
 class testScene : public Scene
 {
 public:
+<<<<<<< HEAD
 	testScene(UIRenderer& uiRender, ObjectRender& objRender, DecalShadow& decalShadow, TempMeshStorage& meshStorage);
+=======
+	testScene(UIRenderer& uiRender, ObjectRender& objRender, TempMeshStorage& meshStorage);
+	
+>>>>>>> master
 	// Inherited via Scene
 	virtual void Load() override;
-
 	virtual void Shutdown() override;
-
-
 	virtual SceneState Update() override;
-
-
 	virtual void Render() override;
 
+private:
 
 	Character* joy;
 	Object* ground;
-	Object* bg;
-	//CharacterCamera* camera;
+	Object* gatoKubo;
 
-	CharacterCamera* camera;
-	ID3D11Buffer* camCb;
-	DirectX::XMFLOAT4X4 viewAndProj;
+	Camera* activeCamera;
+	FreelookCamera* freeCamera;
+	CharacterCamera* joyCamera;
 
 	Object* collTest;
 	Collision coll;
-
 };
