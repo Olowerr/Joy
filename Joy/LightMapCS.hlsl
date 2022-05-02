93dc78e230ab6efd56cc5d17db62c7a2d4a317ee
ADAM ÅKESSON <adao20@student.bth.se>
2022-05-02 11:32:29 +0200

    Missing files fixed

--- Joy/LightMapCS.hlsl
+++ Joy/LightMapCS.hlsl
@@ -1,6 +1,6 @@
 
-RWTexture2DArray<unorm float> lightMapsUAV : register(u0);
-Texture2DArray<unorm float> lightMapsSRV : register(t0);
+RWTexture2D<unorm float> lightMapsUAV : register(u0);
+Texture2D<unorm float> lightMapsSRV : register(t0);
 
 #define NumThreadX 16
 #define NumThreadY 16
@@ -33,13 +33,13 @@ void main( uint3 DTid : SV_DispatchThreadID )
 		}
 	}*/
 
-	if (lightMapsSRV.Load(int4(DTid.xy, 0,0)).r < 0.1)
+	if (lightMapsSRV.Load(int3(DTid.xy, 0)).r < 0.1)
 	{
 		for (int x = -SearchRadius; x <= SearchRadius; x++)
 		{
 			for (int y = -SearchRadius; y <= SearchRadius; y++)
 			{
-				foundValue = lightMapsSRV.Load(int4(DTid.x + x, DTid.y + y, 0,0)).r;
+				foundValue = lightMapsSRV.Load(int3(DTid.x + x, DTid.y + y, 0)).r;
 				if (foundValue > 0.1)
 				{
 					foundCount++;
