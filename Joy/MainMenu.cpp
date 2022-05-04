cd1b21bcc57130cb66b0f54d4b1a2fcd2ad63d08
ADAM ÅKESSON <adao20@student.bth.se>
2022-05-04 09:53:27 +0200

    5252

--- Joy/MainMenu.cpp
+++ Joy/MainMenu.cpp
@@ -35,7 +35,7 @@ MainMenu::MainMenu(UIRenderer& uiRender, ObjectRender& objRender, DecalShadow& d
     obstacle1->SetPosition(0.7f, 1.5f, 20.0f);
     obstacle2->SetPosition(7.5f, 1.5f, 20.0f);
 
-    hLight.GenerateLightMaps();
+    hLight.GenerateLightMaps(divider);
     hLight.ShutdownTools();
 
     objRender.SetActiveCamera(activeCamera);
