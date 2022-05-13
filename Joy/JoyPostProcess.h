d95f3a8460998365552eb1c91a6bcebdf8edb1b1
Olowerr <oliver.klingspor@hotmail.com>
2022-05-13 09:02:36 +0200

    bro

---
+++ Joy/JoyPostProcess.h
@@ -0,0 +1,23 @@
+#pragma once
+#include "Backend.h"
+#include "PostProcess.h"
+
+class JoyPostProcess
+{
+public:
+	JoyPostProcess();
+	void Shutdown();
+
+	void ApplyGlow();
+
+	const UINT NumThreadX = 16;
+	const UINT NumThreadY = 9;
+
+private:
+	ID3D11ComputeShader* blurXCS;
+	ID3D11ComputeShader* blurYCS;
+
+	ID3D11UnorderedAccessView* blurUAV;
+	ID3D11ShaderResourceView* blurSRV;
+
+};
